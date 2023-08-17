be539bcea458306def55cebb0fe7e0d7a79a20a9
Mark Falcone <mark.falcone@abbott.com>
2023-08-16 23:27:54 -0400

    on register

--- bigcorp-lotteries-client/src/onRegisterClick.js
+++ bigcorp-lotteries-client/src/onRegisterClick.js
@@ -4,8 +4,50 @@ export async function onRegisterClick() {
       document.querySelectorAll("input[type=checkbox]")
     );
   
-    // TODO: Register the user for each selected lottery using the POST /register endpoint.
-    // 1. Use the `fetch` API to make the request.
-    // 2. Obtain the user's name from the `nameInput` element.
-    // 3. Check status of the lottery checkboxes using the `checked` property.
+    const currName = nameInput.value;
+    const isAnyChecked = checkboxes.some((checkbox) => checkbox.checked);
+  
+        //     const response = await fetch("http://localhost:3000/register", {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //           name,
+    //           lotteries: checkboxes
+    //             .filter((checkbox) => checkbox.checked)
+    //             .map((checkbox) => checkbox.id),
+    //         }),
+    //       });
+    //       const data = await response.json();
+    //       console.log(data);
+    //       checkboxes.forEach((checkbox) => (checkbox.checked = false));
+    //       nameInput.value = "";
+    //     }
+    if (currName && isAnyChecked) {
+      try {
+        await Promise.all(
+          checkboxes
+            .filter((checkbox) => checkbox.checked)
+            .map((checkbox) =>
+              fetch("http://localhost:5173/register", {
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                  lotteryId: checkbox.id,
+                  name: currName,
+                }),
+              })
+            )
+        );
+  
+        nameInput.value = "";
+        alert(`Successfully registered ${currName} for the selected lotteries!`);
+      } catch (e) {
+        console.error("Error registering for lotteries");
+      }
+    }
   }
